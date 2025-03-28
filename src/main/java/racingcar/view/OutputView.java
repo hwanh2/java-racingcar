4e13faf27b9f03bf6d6cc028e871e6ac671a369b
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-28 23:19:00 +0900

    output 뷰 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -12,22 +12,23 @@ public class OutputView {
 
     public static void printScore(Cars cars) {
         for(Car car : cars.getCars()){
-            System.out.println(car.getName()+" : ");
+            System.out.print(car.getName()+" : ");
             printPosition(car);
-            System.out.println();
         }
+        System.out.println();
     }
 
     public static void printPosition(Car car){
         for(int i=0; i<car.getPosition(); i++){
             System.out.print("-");
         }
+        System.out.println();
     }
 
     public static void printWinners(List<String> winners){
         System.out.print("최종 우승자 : ");
         for(int i=0; i<winners.size(); i++){
-            System.out.println(winners.get(i));
+            System.out.print(winners.get(i));
             if(i<winners.size()-1){
                 System.out.print(", ");
             }
